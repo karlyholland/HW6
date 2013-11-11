3feeab298f991a22ad8406fa11a533083c41c1d7
karlyholland <karly-holland@uiowa.edu>
2013-11-11 16:24:44 -0600

    added more db records

--- default.aspx.vb
+++ default.aspx.vb
@@ -2,12 +2,19 @@
 Partial Class _default
     Inherits System.Web.UI.Page
 
+    'Protected Sub tbSearch_TextChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles tbSearch.TextChanged
+
+    '    Dim searchWord As String
+    '    searchWord = "Select * From [Burger] where (resName Like '%" + tbSearch.Text.ToString() + "%')"
+    '    SqlDataSource1.SelectCommand = searchWord
+
+    'End Sub
+
     Protected Sub tbSearch_TextChanged(sender As Object, e As EventArgs) Handles tbSearch.TextChanged
-
         Dim searchWord As String
-        searchWord = "Select * From Burger where (resName Like '%" & tbSearch.Text.ToString() & "%')"
+        searchWord = "Select * From [Burger] Where (resName Like '%" & tbSearch.Text.ToString() & "%')"
         SqlDataSource1.SelectCommand = searchWord
-
     End Sub
 
 End Class
+
