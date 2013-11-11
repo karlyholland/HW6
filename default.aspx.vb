7edc9dad1287cb18271d84874a3a207446476d09
karlyholland <karly-holland@uiowa.edu>
2013-11-10 19:26:07 -0600

    initial commit

---
+++ default.aspx.vb
@@ -0,0 +1,11 @@
+﻿
+Partial Class gridview
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+
+        lblCopy.Text = DateTime.Now.Year
+
+    End Sub
+
+End Class
---
+++ newrecipe.aspx.vb
@@ -0,0 +1,22 @@
+﻿
+Partial Class detailsview
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+
+        lblCopy.Text = DateTime.Now.Year
+
+    End Sub
+
+    'Protected Sub DetailsView1_ItemInserted(sender As Object, e As DetailsViewInsertedEventArgs) Handles DetailsView1.ItemInserted
+
+    '    Response.Redirect("./default.aspx")
+
+    'End Sub
+    'Protected Sub DetailsView1_ItemUpdated(sender As Object, e As DetailsViewUpdatedEventArgs) Handles DetailsView1.ItemUpdated
+
+    '    Response.Redirect("./default.aspx")
+
+    'End Sub
+
+End Class
